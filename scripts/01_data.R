703f0c4128ad716bbbbc95f02ca74c8ebfe09900
Jingo <joshuaclingo@gmail.com>
2020-12-12 12:08:54 -0800

    Added script to open and clean data

--- scripts/01_data.R
+++ scripts/01_data.R
@@ -1,10 +1,4 @@
-## This script generates example data
-
-N = 50
-seed = 2020-12-02
-
-mu = 0
-sigma = 1
+## This script cleans the data and stores it for later analysis
 
 data_dir = file.path('..', 'data')
 
@@ -12,10 +6,23 @@ if (!dir.exists(data_dir)) {
     dir.create(data_dir)
 }
 
-## Generate data ----
-set.seed(seed)
-dataf = data.frame(x = 1:N, 
-           y = rnorm(N, mu, sigma))
+## Load data ----
+
+dataf_raw = read.csv(file.path(data_dir, 'aggregate-data.csv'))
+
+## Strip out incomplete records
+
+#' Note: Political data is quite incomplete (more than 1400 records are missing this)
+#'    We will be excluding it from the analysis for this reason
+
+#' Note: RelID sometimes came through a textual description but this was rare 
+#'    enough that we'll just exclude those with the other malformed rows
+
+dataf = dataf_raw %>% 
+    subset(select = -c(PoliticsC, X)) %>% 
+    filter(Error != 'NA', BiG100 != 'NA', GenFem != 'NA', Age != 'NA', Education != 'NA', SES != 'NA', is.integer(RelID))
+
+## All in all, we've lost a few hundred rows and the results from China (mainland), which used nonstandard religious IDs
 
 ## Write output ----
 saveRDS(dataf, file.path(data_dir, '01_data.Rds'))
